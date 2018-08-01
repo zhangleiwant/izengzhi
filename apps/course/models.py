66408d0769b3980238a26e7d5f39a8a621167834
zhanglei <lei.zhang@chejianer.com>
2018-08-01 14:28:02 +0800

    “创建课程机构数据库 git commit -m“创建课程机构数据库

--- apps/course/models.py
+++ apps/course/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=50, verbose_name=u'课程名字')
+    desc = models.CharField(max_length=200, verbose_name=u'课程描述')
+    detail = models.TextField(max_length=200, verbose_name=u'课程详情')
+    degree = models.CharField(max_length=2, choices=(('cj', u'初级'), ('zj', u'中级'), ('gj', u'高级')),
+                              verbose_name=u'课程难度')
+    learn_duration = models.IntegerField(default=0, verbose_name=u'学习时长(分钟数)')
+    student_num = models.IntegerField(default=0, verbose_name=u'学习人数')
+    favorites_num = models.IntegerField(default=0, verbose_name=u'收藏人数')
+    image = models.ImageField(max_length='200', upload_to='course/%Y/%m', verbose_name=u'封面图')
+    click_num = models.IntegerField(default=0, verbose_name=u'点击数')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'课程信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    # 外键 TypeError: __init__() missing 1 required positional argument: 'on_delete'
+    course = models.ForeignKey(Course, verbose_name=u'所属课程',on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name=u'章节名称')
+    add_time = models.IntegerField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'章节信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class Video(models.Model):
+    lesson = models.ForeignKey(Lesson, verbose_name=u'所属章节',on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name=u'视频名称')
+    dd_time = models.IntegerField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'视频信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class CourseResource(models.Model):
+    course = models.ForeignKey(Course, verbose_name=u'所属课程',on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name=u'课程资源名')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+    download = models.FileField(max_length=100, upload_to='course/resource/%Y/%m', verbose_name=u'课程资源文件')
+
+    class Meta:
+        verbose_name = u'课程资源'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
--- apps/organization/models.py
+++ apps/organization/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from datetime import datetime
+
+
+class CityDict(models.Model):
+    name = models.CharField(max_length=20, verbose_name=u'城市名字')
+    desc = models.CharField(max_length=200, verbose_name=u'城市描述')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'城市信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
+class CourseOrg(models.Model):
+    name = models.CharField(max_length=50, verbose_name=u'机构名字')
+    desc = models.CharField(max_length=100,verbose_name=u'机构描述')
+    image = models.ImageField(max_length='200', upload_to='course/%Y/%m', verbose_name=u'封面图')
+    favorites_num = models.IntegerField(default=0, verbose_name=u'收藏人数')
+    click_num = models.IntegerField(default=0, verbose_name=u'点击数')
+    address = models.CharField(max_length=200, verbose_name=u'机构地址')
+    city = models.ForeignKey(CityDict, verbose_name=u'所属城市',on_delete=models.CASCADE)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'课程机构信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class Teacher(models.Model):
+    org = models.ForeignKey(CourseOrg, verbose_name=u'所属机构',on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, verbose_name=u'教师名字')
+    work_years = models.IntegerField(default=0, verbose_name=u'工作年限')
+    work_company = models.CharField(max_length=50, verbose_name=u'就职公司')
+    work_position = models.CharField(max_length=50, verbose_name=u'工作岗位')
+    work_point = models.CharField(max_length=100, verbose_name=u'教学特点')
+    favorites_num = models.IntegerField(default=0, verbose_name=u'收藏人数')
+    click_num = models.IntegerField(default=0, verbose_name=u'点击数')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'课程教师信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
