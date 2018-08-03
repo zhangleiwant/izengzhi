bd3c921896a884d60726528a26874bbaf5aae537
zhanglei <lei.zhang@chejianer.com>
2018-08-03 14:48:02 +0800

    自定义认证实现用户名邮箱手机登录

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.backends import ModelBackend
+from .models import UserProfile
+from django.db.models import Q
+
+
+# 自定义authentication
+class CustomBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = UserProfile.objects.get(Q(username=username) | Q(email=username) | Q(mobile=username))  # 获取用户
+            if user.check_password(password):  # 检测密码
+                return user
+        except Exception as e:
+            return None
 
 
 # Create your views here.
@@ -8,11 +22,11 @@ def user_login(request):
         user_name = request.POST.get('username', '')
         pass_word = request.POST.get('password', '')
         user = authenticate(username=user_name, password=pass_word)
-        if user:
+        if user is not None:
             login(request, user)
             return render(request, 'index.html')
         else:
-            return render(request, 'login.html', {})
+            return render(request, 'login.html', {'msg': '用户名或密码错误'})
         pass
     elif request.method == 'GET':
         return render(request, 'login.html', {})
--- izengzhi/settings.py
+++ izengzhi/settings.py
@@ -130,3 +130,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+AUTHENTICATION_BACKENDS = (
+    'apps.user.views.CustomBackend',
+)
